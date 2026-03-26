8810cd577d3dfc69c46c68f19ff28f1fce5f1b2a
Anurag <anuragchakarde@gmail.com>
2026-03-26 22:01:15 +0530

    tokenization : stopwords removed

--- bm25_search.py
+++ bm25_search.py
@@ -1,26 +1,43 @@
 import json
+import re
 from rank_bm25 import BM25Okapi
+from nltk.corpus import stopwords
+
+stop_words = set(stopwords.words("english"))
+
+
+def tokenize(text):
+    # lowercase
+    text = text.lower()
+
+    # remove punctuation
+    text = re.sub(r'[^a-z0-9\s]', '', text)
+
+    # split
+    tokens = text.split()
+
+    # remove stopwords
+    tokens = [word for word in tokens if word not in stop_words]
+
+    return tokens
 
 
 class BM25Search:
     def __init__(self, docs):
         self.documents = docs
 
-        # tokenize documents
         self.corpus = [
-            doc["content"].lower().split()
+            tokenize(doc["content"])
             for doc in docs
         ]
 
-        # build BM25 index
         self.bm25 = BM25Okapi(self.corpus)
 
     def search(self, query, top_k=5):
-        tokenized_query = query.lower().split()
+        tokenized_query = tokenize(query)
 
         scores = self.bm25.get_scores(tokenized_query)
 
-        # rank results
         ranked = sorted(
             zip(self.documents, scores),
             key=lambda x: x[1],
--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-from crawler.crawler import Crawler
+# from crawler.crawler import Crawler
+# import json
+
+# SEEDS = [
+#     "https://old.reddit.com/r/programming/",
+#     "https://medium.com/tag/programming",
+#     "https://github.com/topics/programming"
+# ]
+
+# crawler = Crawler(SEEDS, max_pages=200, max_depth=2)
+# docs = crawler.crawl()
+
+# with open("data/documents.json", "w") as f:
+#     json.dump(docs, f, indent=2)
+
+# print(f"Collected {len(docs)} documents")
+
 import json
+from crawler import crawler
+from bm25_search import BM25Search
 
-SEEDS = [
-    "https://old.reddit.com/r/programming/",
-    "https://medium.com/tag/programming",
-    "https://github.com/topics/programming"
-]
+# load documents
+with open("data/documents.json") as f:
+    docs = json.load(f)
 
-crawler = Crawler(SEEDS, max_pages=200, max_depth=2)
-docs = crawler.crawl()
+search_engine = BM25Search(docs)
 
-with open("data/documents.json", "w") as f:
-    json.dump(docs, f, indent=2)
+query = input("Search: ")
 
-print(f"Collected {len(docs)} documents")
+results = search_engine.search(query)
+
+for doc, score in results:
+    print("\n---")
+    print("Score:", score)
+    print("Title:", doc["title"])
+    print("URL:", doc["url"])
